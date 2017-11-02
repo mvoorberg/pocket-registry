310194d90f6eae673371ebcbd4ddc02a5e7f7ed2
Mark Voorberg <mark@binaryops.ca>
2017-11-02 07:47:47 -0700

    fixing up the release script

--- release.js
+++ release.js
@@ -1,10 +1,14 @@
 const shell = require('shelljs');
 
-if (exec('git status --porcelain').output) {
-    console.error('Git working directory not clean.');
+// Is the repos all checked-in?
+const isClean = exec('git status --porcelain');
+if (isClean.stdout || isClean.stderr) {
+    console.error('Git working directory is not clean.');
+    console.error(isClean.stdout || isClean.stderr);
     process.exit(2);
 }
 
+// Get major/minor/patch from the command line
 const versionIncrement = process.argv[process.argv.length - 1];
 
 if (versionIncrement != 'major' && versionIncrement != 'minor' && versionIncrement != 'patch') {
@@ -12,22 +16,24 @@ if (versionIncrement != 'major' && versionIncrement != 'minor' && versionIncreme
     process.exit(1);
 }
 
+// Increment the version in package.json
 exec('npm version ' + versionIncrement);
 
+// Run tests and deploy to npm.
 exec('npm test');
 exec('git push');
 exec('git push --tags');
 exec('npm publish');
 
+// Run commands quietly, log output and exit only on errors.
 function exec(cmd) {
     const ret = shell.exec(cmd, {
         silent: true
     });
 
     if (ret.code != 0) {
-        console.error(ret.output);
+        console.error("Error:", ret.stdout, ret.stderr);
         process.exit(1);
     }
-
     return ret;
 }
